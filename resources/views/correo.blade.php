9789d994e970743a09f9a11ee972cc125fdbe584
daververa99 <daververa99@gmail.com>
2024-01-27 16:38:49 -0500

    first commit

---
+++ resources/views/correo.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Mi Correo Electrónico</title>
+    <style>
+        body {
+            width: 600px;
+            height: 600px;
+            margin: 0 auto;
+            background: white;
+            box-shadow: 0 0 5px rgba(0, 0, 0, 0.1);
+        }
+    </style>
+</head>
+
+<body>
+    <h1>¡Hola!</h1>
+    <p>Este es un ejemplo de una plantilla de correo electrónico HTML.</p>
+</body>
+
+</html>
---
+++ resources/views/factura.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Mi Documento A4</title>
+    <style>
+        body {
+            width: 210mm;
+            height: 297mm;
+            margin: 0 auto;
+            background: white;
+            box-shadow: 0 0 5px rgba(0, 0, 0, 0.1);
+        }
+    </style>
+</head>
+
+<body>
+    <h1>Bienvenido a mi página A4</h1>
+    <p>Este es un ejemplo de una página HTML que se ajusta al tamaño de una hoja A4.</p>
+</body>
+
+</html>
